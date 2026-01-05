fe8f1161ccb9b1f47f93edf71db3ed9725698c70
YY <mightybj10@gmail.com>
2026-01-05 15:02:48 +0000

    My First C Program
    Basic structure, comments and printf

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# c-playground
-Purpose: learning + experimentation
+# C-Playground 🛝
+I will use this repository to track my progress as I learn C programming. After learning a section and reviewing it from the tutorial, I will recreate it in my ✨own style✨ and publish it here.
+
+**Tutorial Link:**
+https://www.youtube.com/watch?v=xND0t1pr3KY
+
+## Topics Learned (So Far)
+- BLANK
+- BLANK
---
+++ hello.c
@@ -0,0 +1,18 @@
+// My First C Program - basic structure, comments and printf
+#include <stdio.h>
+
+int main(){
+    // HOLAAAAAAAAAAAAAAAAA
+
+    /* 
+    testing testing testing
+    yo hellooo?
+    */
+
+    // ALWAYS remember to end line of code with semi-colon.
+    printf("If you like chicken nuggets then you gotta sing along 🎵");
+    printf("If you like chicken nuggets this is your fav song 🎶");
+
+    // ALWAYS return 0 at the end of function
+    return 0; // Confirms if program ran successfully 
+}
